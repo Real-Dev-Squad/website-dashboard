25483a766b76aab0934d0883c117f8bcd7bc3b37
José Machado <josedemouramachado@gmail.com>
2021-04-14 22:29:02 +0100

    Changed the from 'true' to 'on'

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ taskForm.onsubmit = async (e) => {
       dinero: lossRateDinero,
       neelam: lossRateNeelam,
     },
-    isNoteworthy: isNoteworthy = (isNoteworthy == 'true'),
+    isNoteworthy: isNoteworthy = (isNoteworthy == 'on'),
   };
 
   if (dataToBeSent.type == 'feature') {
