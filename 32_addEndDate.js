3428f1d5dcbe11af329bb79269beb820d25e8c43
Aditi Singh <singhaditi.riya25@gmail.com>
2021-06-28 21:46:17 +0530

    adding dynamic end date

--- 32_addEndDate.js
+++ 32_addEndDate.js
@@ -1,16 +1,5 @@
-Date.prototype.addDays = function(days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-}
+{var date = new Date();
+console.log(date);
+date.setDate(date.getDate() + 7);
 
-$('#txtStartDate').datepicker();
-$('#txtEndDate').datepicker();
-
-$('#txtStartDate').change(function(){
-    var interval = 7;
-    var startDate = new Date($(this).val());
-    $('#txtEndDate').val(startDate.addDays(interval)
-                        );
-}
-                         );
+var finalDate = date.getDate()+'/'+ (date.getMonth()+1) +'/'+date.getFullYear();}
