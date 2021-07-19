2a649c38592c13704e8e95fc5e7a0d7fc448eaf2
Aditi Singh <86421817+Aditi-0725@users.noreply.github.com>
2021-07-19 19:54:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -188,6 +188,6 @@ const dd = String(currentDate.getDate()).padStart(2, '0');
 const mm = String(currentDate.getMonth() + 1).padStart(2, '0');
 const yyyy = currentDate.getFullYear();
 
-let today = `${yyyy}-${mm}-${dd}`; 
+const today = `${yyyy}-${mm}-${dd}`; 
 startedDate.value = today;
 setEndDate(currentDate);
