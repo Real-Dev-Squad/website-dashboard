e711845865c0466c255e5688a99c705c8bc63030
pavangbhat <pavanbhat17@gmail.com>
2021-11-18 18:45:53 +0530

    created server to serve static files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
