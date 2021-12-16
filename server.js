849df76371d717ed03df036219c84adf690709a6
pavangbhat <pavanbhat17@gmail.com>
2021-12-16 11:38:03 +0530

    made few changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('src'));
 
 app.get('/', (req, res) => {
   res.render('index.html');
