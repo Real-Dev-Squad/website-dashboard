7bf92db2886a4c4181050726f14264fead23a39b
pavangbhat <pavanbhat17@gmail.com>
2021-12-24 15:19:38 +0530

    changed BASE_URL to API_BASE_URL

--- src/helpers/loadENV.js
+++ src/helpers/loadENV.js
@@ -1,3 +1,3 @@
 window.location.hostname !== 'dashboard.realdevsquad.com'
-  ? (window.BASE_URL = 'https://staging-api.realdevsquad.com')
-  : (window.BASE_URL = 'https://api.realdevsquad.com');
+  ? (window.API_BASE_URL = 'https://staging-api.realdevsquad.com')
+  : (window.API_BASE_URL = 'https://api.realdevsquad.com');
