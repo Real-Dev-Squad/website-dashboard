1a152356b9d9217206a84120fc049731793e370d
pavangbhat <pavanbhat17@gmail.com>
2022-02-08 23:55:25 +0530

    refactored remaining days logic

--- src/helpers/loadENV.js
+++ src/helpers/loadENV.js
@@ -1,3 +1,3 @@
 window.location.hostname !== 'dashboard.realdevsquad.com'
-  ? (window.API_BASE_URL = 'https://staging-api.realdevsquad.com')
+  ? (window.API_BASE_URL = 'http://localhost:3001')
   : (window.API_BASE_URL = 'https://api.realdevsquad.com');
