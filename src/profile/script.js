d34c6a1484ecd5fd1d632b8863de0b81261c1337
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-04-04 20:24:04 +0530

    fixed lint errors

--- src/profile/script.js
+++ src/profile/script.js
@@ -105,7 +105,7 @@ function createCard({ oldData, newData, userId, username, profileDiffId }) {
 
   approveBtn.onclick = async () => {
     const reason = prompt('Reason for Approval');
-    if(reason != NULL){
+    if (reason != NULL) {
       document.getElementById('cover-spin').style.display = 'block';
       try {
         const response = await fetch(`${API_BASE_URL}/users/${userId}`, {
@@ -146,7 +146,7 @@ function createCard({ oldData, newData, userId, username, profileDiffId }) {
 
   rejectBtn.onclick = async () => {
     const reason = prompt('Reason for Rejection');
-    if(reason != NULL){
+    if (reason != NULL) {
       document.getElementById('cover-spin').style.display = 'block';
       try {
         const response = await fetch(`${API_BASE_URL}/users/rejectDiff`, {
