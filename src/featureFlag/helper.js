71522dd902f16a56693608a16d3ab94970fab293
Pallab Sonowal <pallabsonowal999@gmail.com>
2022-04-23 16:39:54 +0530

    add functionality and validation to form

--- src/featureFlag/helper.js
+++ src/featureFlag/helper.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const API_BASE_URL = 'https://api.realdevsquad.com';
+const roles = ['super_user', 'member', 'no_role'];
 const mockData = [
   {
-    flagId: 1,
+    flagId: '111',
     title: 'Dark Mode',
     enabled: true,
     roles: ['members', 'no_role', 'super_user'],
@@ -13,7 +14,7 @@ const mockData = [
     },
   },
   {
-    flagId: 2,
+    flagId: '222',
     title: 'Light Mode',
     enabled: false,
     roles: [],
@@ -23,6 +24,13 @@ const mockData = [
     },
   },
 ];
+const newFlagConfig = {
+  flagId: '',
+  title: '',
+  enabled: true,
+  roles: [],
+  users: {},
+};
 
 function customCreateElement({ type = 'div', attribute = {}, text = null }) {
   const element = document.createElement(type);
@@ -32,50 +40,112 @@ function customCreateElement({ type = 'div', attribute = {}, text = null }) {
     element.setAttribute(key, attribute[key]);
   });
 
-  if (text) {
-    element.textContent = text;
-  }
+  if (text) element.textContent = text;
 
   return element;
 }
 
+// For mocking
+const awaitTimeout = (delay) => {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+};
+
 async function fetchFlags() {
   // const data = await fetch(`${API_BASE_URL}/flag`).then(res => res.json());
   // return data.flags;
+  await awaitTimeout(2000);
   return mockData;
 }
 
-function renderFlags(flags) {
+function setFlag(flag) {
+  // Show the flag title name with Read Only
+  // or remove flag name and remove readonly attribute
+  const titleField = document.getElementById('flag-title');
+  titleField.value = flag.title;
+  flag.flagId
+    ? titleField.setAttribute('readonly', '')
+    : titleField.removeAttribute('readonly');
+
+  // Sync up role checkboxes
+  roles.forEach((value) => {
+    const roleField = document.getElementById(`checkbox-${value}`);
+    roleField.checked = flag.roles.includes(value);
+  });
+
+  // Sync up enable checkbox
+  const enabledField = document.getElementById('form-enabled');
+  enabledField.checked = flag.enabled;
+
+  // Sync flagId
+  document.getElementById('form-flagId').value = flag.flagId;
+}
+
+function renderFlagDropdown(flags) {
+  flags.forEach((flag) => appendFlagToDropdown(flag));
+}
+
+function appendFlagToDropdown(flag) {
   const flagDropdown = document.getElementById('flag-dropdown');
-  flags.forEach((flag) => {
-    const element = customCreateElement({
-      type: 'option',
-      attribute: { value: flag.flagId },
-      text: flag.title,
-    });
-    flagDropdown.append(element);
+  const element = customCreateElement({
+    type: 'option',
+    attribute: { value: flag.flagId },
+    text: flag.title,
   });
-}
-
-// Listeners
-function configureListeners() {
-  handleAddFlagButtonClick();
-}
-
-function handleAddFlagButtonClick() {
-  const addFlagBtn = document.querySelector('.add-button');
-  addFlagBtn.addEventListener('click', (e) => {
-    e.target.classList.add('add-button--active');
-  });
+  flagDropdown.append(element);
 }
 
 // Submit
-function getFlagFormData(form, applicationState) {
+function getFlagFormData(form) {
   const data = new FormData(form);
   const result = {};
 
+  result.flagId = data.get('flagId');
   result.title = data.get('title');
   result.roles = data.getAll('role');
   result.enabled = data.get('enabled') ? true : false;
+  result.users = {}; // TODO
   return result;
+}
+
+function toggleSubmitButton({ enable }) {
+  const submitBtn = document.getElementById('form-submit');
+  if (enable) {
+    submitBtn.removeAttribute('disabled');
+    submitBtn.classList.remove('submit-button__disabled');
+  } else {
+    submitBtn.setAttribute('disabled', true);
+    submitBtn.classList.add('submit-button__disabled');
+  }
+}
+
+async function updateFlagCall(flagId, data) {
+  // Call the post /flag/:flagId api
+  await awaitTimeout(2000);
+  const index = flags.findIndex((flag) => flag.flagId === flagId);
+  flags[index] = { flagId, ...data };
+
+  alert('Flag updated successfully');
+  toggleSubmitButton({ enable: true });
+}
+
+let mockFlagId = 1;
+async function addFlagCall(data) {
+  const validFlagTitle = validateFlagTitle(data.title);
+  if (!validFlagTitle) {
+    alert('Error! Flag title already exists');
+    toggleSubmitButton({ enable: true });
+    return;
+  }
+  // Call the post /flag api & disable submit button
+  await awaitTimeout(2000);
+  const flag = { flagId: `${mockFlagId++}`, ...data };
+  appendFlagToDropdown(flag);
+  flags.push(flag);
+
+  alert('Flag added successfully');
+  toggleSubmitButton({ enable: true });
+}
+
+function validateFlagTitle(title) {
+  return !flags.some((flag) => flag.title === title);
 }
--- src/featureFlag/script.js
+++ src/featureFlag/script.js
@@ -1,26 +1,42 @@
-let applicationState = 'ADD_FLAG'; // ADD_FLAG UPDATE_FLAG
+'use strict';
+
+let flags;
 
 // initialize
 async function initialize() {
-  const flags = await fetchFlags();
-  renderFlags(flags);
-  configureListeners();
+  flags = await fetchFlags();
+  renderFlagDropdown(flags);
   handleOnSubmit();
 }
-
 initialize();
 
-// On selecting flag
+// On selecting/changing flag
+async function handleFlagChange(e) {
+  const flagId = e.value;
+  const flag = flags.find((flag) => flag.flagId === flagId);
+
+  setFlag(flag);
+  document.getElementById('create-flag').style.display = 'block';
+}
 
 // On selecting Add button
+async function handleAddFlag() {
+  document.getElementById('flag-dropdown-default').selected = 'selected';
+  setFlag(newFlagConfig);
+
+  document.getElementById('create-flag').style.display = 'none';
+  console.log(document.getElementById('create-flag'));
+}
 
 // Submitting form
-function handleOnSubmit() {
+async function handleOnSubmit() {
   const flagForm = document.querySelector('#flag-form');
   flagForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const data = getFlagFormData(flagForm, applicationState);
-    console.log(data);
+    toggleSubmitButton({ enable: false });
+    // Using flagId to determine weather we want to add or update flag
+    const { flagId, ...data } = getFlagFormData(flagForm);
+    flagId ? updateFlagCall(flagId, data) : addFlagCall(data);
   });
 }
