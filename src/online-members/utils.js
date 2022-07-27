34ec78726f6d66449ae858c4ed1fd7608eae3d6d
Prakash <om.pcprakash@gmail.com>
2022-07-27 21:49:54 +0530

    Added div around input, refactored search function

--- src/online-members/utils.js
+++ src/online-members/utils.js
@@ -47,20 +47,16 @@ const getCloudinaryImgURL = (publicId, configs) =>
   `${RDS_CLOUDINARY_CLOUD_URL}${configs ? `/${configs}` : ''}/${publicId}`;
 
 function searchFunction() {
-  // Declare variables
   let divText, i, txtValue;
   const input = document.getElementById('searchMembers');
   const filter = input.value.toUpperCase();
   const ul = document.getElementById(MEMBERS_LIST_ID);
   const li = ul.getElementsByTagName('li');
-
   for (i = 0; i < li.length; i++) {
     divText = li[i].getElementsByTagName('div')[0];
     txtValue = divText.textContent || divText.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = '';
-    } else {
-      li[i].style.display = 'none';
-    }
+    const displayText =
+      txtValue.toUpperCase().indexOf(filter) > -1 ? '' : 'none';
+    li[i].style.display = displayText;
   }
 }
