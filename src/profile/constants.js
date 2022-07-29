0d71fac6290fd1013e52d0903fe986f8a06a1645
OJAS ELAWADHI <83074474+ojas-elawadhi@users.noreply.github.com>
2022-07-29 23:02:22 +0530

    Fix the data going in body of the approve button call (#157)
    * Fix the data going in body of the approve button call
    
    * Fix the data going in body of the approve button call
    
    * updated minor changes
    
    * updated yoe data going
    
    * improvements in code structure
    
    * improvements in code structure
    
    * improvements in code structure
    
    * removed irrelevant comments and moved createElement in a reusable method
    
    Co-authored-by: Ankush Dharkar <ankushdharkar@users.noreply.github.com>

---
+++ src/profile/constants.js
@@ -0,0 +1,9 @@
+export const API_BASE_URL = 'https://api.realdevsquad.com';
+export const YEARS_OF_EXPERIENCE = 'yoe';
+export const SUPER_USER = 'super_user';
+export const APPROVE_BUTTON_TEXT = 'Approve';
+export const REJECT_BUTTON_TEXT = 'Reject';
+export const APPROVAL_PROMPT_TEXT = 'Reason for Approval';
+export const ALERT_APPROVED = 'User Data Approved !!!';
+export const ALERT_ERROR = 'Something went wrong. Please check console errors.';
+export const ALERT_REJECTED = 'User Data Rejected!!!';
