570ebb9f8ec6af1ebc6154b49146108fd5565639
Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>
2022-09-30 23:23:35 +0530

    get task of users from the list (#190)
    * get task of users from the list
    
    * Added div around input, refactored search function
    
    * get task of users from the list
    
    * Added div around input, refactored search function
    
    * Chore-Refactor-code
    
    * Chore-deleted duplicte fles
    
    * chore: created constants for some hard coded messages
    
    Co-authored-by: Harshith Venkatesh <harshithece7016@gmail.com>

---
+++ src/online-members/constants.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const MEMBERS_CLASS_LIST = ['members'];
+const MEMBERS_UL_CLASS_LIST = ['members-list'];
+const MEMBERS_LIST_ID = 'members-list';
+const MEMBERS_CONTAINER_CLASS_LIST = ['members-container'];
+const MEMBERS_CONTAINER_ID = 'members-container';
+
+const TASKS_CONTAINER_ID = 'task-container';
+const TASKS_CLASS_LIST = ['task'];
+const TASKS_CONTAINER_CLASS_LIST = ['tasks-container'];
+
+const MEMBERS_SEARCH_CLASS_LIST = ['members-search'];
+const MEMBERS_SEARCH_INPUT_CLASS_LIST = ['members-search-input'];
+const MEMBERS_SEARCH_ID = 'search-members';
+const MEMBERS_SEARCH_PLACEHOLDER = 'Search for members';
+
+const RDS_API_MEMBERS = 'https://api.realdevsquad.com/members';
+const RDS_API_TASKS_USERS = 'https://api.realdevsquad.com/tasks';
+const RDS_CLOUDINARY_CLOUD_URL = `https://res.cloudinary.com/realdevsquad/image/upload`;
+
+const RDS_PROFILE_IMAGE_SIZE = 'w_40,h_40';
+const RDS_PROFILE_DEFAULT_IMAGE = '';
+
+const PROFILE_IMAGE_CLASS = [];
+
+/* MESSAGES CONSTANTS */
+
+const MESSAGE_SOMETHING_WENT_WRONG =
+  'Something went wrong. Please contact admin';
+const MESSAGE_NO_TASK = 'No tasks found, create some for them please :P';
+const MESSAGE_LOADING = 'LOADING...';
---
+++ src/online-members/utils.js
@@ -0,0 +1,65 @@
+async function makeApiCall(
+  url,
+  method = 'get',
+  body = null,
+  headers = [],
+  options = null,
+) {
+  try {
+    const response = await fetch(url, {
+      method,
+      body,
+      headers,
+      ...options,
+    });
+    return response;
+  } catch (err) {
+    console.error(MESSAGE_SOMETHING_WENT_WRONG, err);
+    return err;
+  }
+}
+
+async function getMembersData() {
+  let membersList = null;
+  const membersRequest = await makeApiCall(RDS_API_MEMBERS);
+  if (membersRequest.status === 200) {
+    membersList = await membersRequest.json();
+    membersList = membersList.members;
+    membersList = membersList.filter((member) => !member.incompleteUserDetails);
+  }
+  return membersList;
+}
+
+async function getMemberTaskData(username) {
+  let taskData = null;
+  const membersRequest = await makeApiCall(rdsApiTaskDetails(username));
+  if (membersRequest.status === 200) {
+    taskData = await membersRequest.json();
+    taskData = taskData.tasks;
+  }
+  return taskData;
+}
+
+const rdsApiTaskDetails = (username = null) =>
+  `${RDS_API_TASKS_USERS}/${username}`;
+
+const getCloudinaryImgURL = (publicId, configs) => {
+  const imageSizeOptions = configs ? `/${configs}` : '';
+  return `${RDS_CLOUDINARY_CLOUD_URL}${imageSizeOptions}/${publicId}`;
+};
+
+function searchFunction() {
+  let divText, txtValue;
+  const input = document.getElementById(MEMBERS_SEARCH_ID);
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById(MEMBERS_LIST_ID);
+  const li = ul.getElementsByTagName('li');
+  const liArray = Array.from(li);
+  liArray.forEach((liItem) => {
+    divText = liItem.getElementsByTagName('div')[0];
+    txtValue = divText.textContent || divText.innerText;
+    const displayStyle =
+      txtValue.toUpperCase().indexOf(filter) > -1 ? '' : 'none';
+    liItem.style.display = displayStyle;
+  });
+}
