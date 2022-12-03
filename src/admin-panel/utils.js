ae9cc16d9f8249294888e8ba7ed21bdf2f22268b
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2022-12-04 00:35:45 +0530

    added admin-panel feature (#249)
    * added admin-panel feature
    
    * added API calls in try catch
    
    * changed the URL
    
    * added loading state
    
    * made the suggested change
    
    * added authrization
    
    * changed the base URL

---
+++ src/admin-panel/utils.js
@@ -0,0 +1,10 @@
+function createElement({ type, attributes = {}, innerText }) {
+  const element = document.createElement(type);
+  Object.keys(attributes).forEach((item) => {
+    element.setAttribute(item, attributes[item]);
+  });
+  element.textContent = innerText;
+  return element;
+}
+
+export { createElement };
