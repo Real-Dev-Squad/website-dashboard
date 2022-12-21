18aa67341d030f96b4080b86cc23f6cf6bfbc762
Vedant Manjrekar <vedant.28m@gmail.com>
2022-12-21 12:15:32 +0530

    Fixed responsiveness issue

--- test.js
+++ test.js
@@ -1,4 +1,38 @@
 const puppeteer = require('puppeteer');
+const { EventEmitter } = require('events');
+
+// Function for testing responsiveness of ".buttonSection"
+function test_btn(width, height, expected_display, test_no) {
+  puppeteer.launch({ headless: true }).then(async (browser) => {
+    const page = await browser.newPage();
+    const emitter = new EventEmitter();
+
+    await page.setViewport({
+      width,
+      height,
+    });
+
+    // for official website
+    // await page.goto('https://dashboard.realdevsquad.com/');
+
+    // for local environment
+    await page.goto('http:localhost:5500');
+
+    const display = await page.$eval(
+      '.buttonSection',
+      (el) => getComputedStyle(el).display,
+    );
+
+    if (display == expected_display) {
+      console.log(`> Test ${test_no} Passed. ✅`);
+    } else {
+      throw new Error(`> Test ${test_no} Failed. ❌`);
+    }
+
+    await browser.close();
+  });
+}
+
 let config = {
   launchOptions: {
     headless: false,
@@ -43,3 +77,18 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(3000);
   await browser.close();
 });
+
+// Test Case1: Testing for Desktop width (900x1020)
+test_btn(900, 1020, 'flex', 1);
+
+// Test Case2: Testing for Mobile width (IphoneX)
+test_btn(414, 896, 'grid', 2);
+
+// Test Case3: Testing for Mobile width (motoG)
+test_btn(360, 640, 'grid', 3);
+
+// Test Case3: Testing for Mobile width (Pixel 5)
+test_btn(393, 851, 'grid', 4);
+
+// Test Case5: Testing for Mobile width (NestHub max)
+test_btn(1280, 800, 'flex', 5);
