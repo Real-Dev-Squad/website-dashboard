3bb1d85e744529e7fac300f96e62f8329c8c6f89
ivinayakg <vinayak20029@gmail.com>
2023-02-01 01:57:17 +0530

    .

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://api.realdevsquad.com';
 const USER_MANAGEMENT_LINK = 'user-management-link';
 const EXTENSION_REQUESTS_LINK = 'extension-requests-link';
