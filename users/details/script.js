bccf74417edf954c2890d5a87b762b43fdfc2678
SahsiSunny <sahsisunny@gmail.com>
2023-02-07 02:15:42 +0530

    function moved to utils.js file for reuse

--- users/details/script.js
+++ users/details/script.js
@@ -61,25 +61,6 @@ async function getUserData() {
   }
 }
 
-function generateNoDataFoundSection(message) {
-  document.title = 'User Not Found';
-  const notFoundDiv = createElement({ type: 'div', classList: ['not-found'] });
-  const notFoundImg = createElement({
-    type: 'img',
-    classList: ['not-found-img'],
-  });
-  notFoundImg.src = '/images/page-not-found.png';
-  notFoundImg.setAttribute('alt', 'page not found');
-  const notFoundText = createElement({
-    type: 'h1',
-    classList: ['not-found-text-h1'],
-  });
-  notFoundText.appendChild(createTextNode(message));
-  notFoundDiv.append(notFoundImg, notFoundText);
-  const container = document.querySelector('.user-details-header');
-  container.appendChild(notFoundDiv);
-}
-
 function generateUserImage(alt) {
   const img = createElement({ type: 'img' });
   img.src = userData.picture?.url ?? defaultAvatar;
