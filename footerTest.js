5c1c0e3f66eeac2ec3e891d579191be27fd85585
Khushi Shukla <khushishukla5890@gmail.com>
2023-03-11 11:19:05 +0530

    feat: footerTest for all urls

--- footerTest.js
+++ footerTest.js
@@ -9,28 +9,28 @@ let config = {
 
 let urls = [
   'https://dashboard.realdevsquad.com/index.html',
-  'https://dev.realdevsquad.com/users/index.html',
-  'https://dev.realdevsquad.com/users/details/index.html',
   'https://dev.realdevsquad.com/profile/index.html',
   'https://dev.realdevsquad.com/goal/index.html',
   'https://dev.realdevsquad.com/task/index.html',
+  'https://dev.realdevsquad.com/users/index.html',
+  'https://dev.realdevsquad.com/users/details/index.html',
   'https://dev.realdevsquad.com/taskevents/index.html',
   'https://dev.realdevsquad.com/featureflag/index.html',
   'https://dev.realdevsquad.com/wallet/index.html',
   'https://dev.realdevsquad.com/online-members/online-members.html',
 ];
 
-const test_footer = (url, index) => {
-  puppeteer.launch(config.launchOptions).then(async (browser) => {
+const test_footer = async (url, index) => {
+  await puppeteer.launch(config.launchOptions).then(async (browser) => {
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url, { waitUntil: ['load', 'networkidle2'] });
     await page.content();
     page.setDefaultNavigationTimeout(0);
     const footer = await page.$('footer');
     // if footer exists
     if (footer) {
       // is the text same in footer?
-      const footer_para = await page.evaluate(() => {
+      const footer_text = await page.evaluate(() => {
         const element = document.querySelector('.info-repo');
         console.log(element);
 
@@ -41,20 +41,30 @@ const test_footer = (url, index) => {
         );
         // will return undefined if the element is not found
       });
-      console.log(`footer text :${footer_para} index : ${index}:${url}`);
-    } else {
-      console.log('no footer');
-    }
+      console.log(`✅ footer text:${footer_text}  index:${index}:${url}`);
 
-    await Promise.all([
-      page.click('.info-repo a', { delay: 2000 }),
-      page.waitForNavigation(),
-      setTimeout(() => {
-        console.log(`links works for ${url}`);
-        browser.close();
-      }, 10000),
-    ]).catch((e) => console.log('disconnected'));
+      if (footer_text) {
+        // links inside footer are working or not
+        await Promise.all([
+          page.click('.info-repo a', { delay: 2000 }),
+          page.waitForNavigation(),
+          setTimeout(() => {
+            console.log(`✅ link works for ${url}`);
+            browser.close();
+          }, 10000),
+        ]).catch((e) => console.log('disconnected '));
+      }
+    } else {
+      console.log(`no footer at ${url}`);
+    }
   });
 };
 
-test_footer(urls[0], 0);
+async function runTest() {
+  for (let url of urls) {
+    console.log(urls.indexOf(url));
+    await test_footer(url, urls.indexOf(url));
+  }
+}
+
+runTest();
