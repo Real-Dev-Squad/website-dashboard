5966bce47681265d77e1ea44a884f3b44f95cbc2
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-04-25 10:09:14 +0530

    changed the babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
-const plugins = [];
-// when run in the CI enviroonment
-const isCI = Boolean(process.env.CI || false);
-// when run locally directly (calling npm start)
-const isDevelopment = process.env.NODE_ENV === 'development';
-// when run in test environment by jest (npm test)
-const isTest = process.env.NODE_ENV === 'test';
-if (isTest || isDevelopment || isCI) {
-  plugins.push('istanbul');
-}
+const plugins = ['istanbul'];
+
 module.exports = {
   plugins: plugins,
 };
