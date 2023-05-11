5c2673ec0584e2820d2dde3bcc40ec70b5c0f92d
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-05-11 12:40:07 +0530

    move files, chnage url

--- users/discord/index.js
+++ users/discord/index.js
@@ -1,4 +1,7 @@
 import { App } from './App.js';
+const urlParams = new URLSearchParams(window.location.search);
+
+if (!urlParams.get('dev')) history.back();
 
 window['root'].lastElementChild?.remove();
 react.render(App(), window['root']);
