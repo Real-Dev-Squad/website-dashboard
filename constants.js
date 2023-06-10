011657e2003d8771b9fb2eb2c86558ca4998462a
SahsiSunny <sahsisunny@gmail.com>
2023-06-10 07:08:19 +0530

    create separate constants file for standup update

--- constants.js
+++ constants.js
@@ -5,5 +5,3 @@ const dummyPicture =
   'https://dashboard.realdevsquad.com/users/images/avatar.png';
 const USER_MANAGEMENT_URL =
   'https://dashboard.realdevsquad.com/users/details/?username=';
-const RDS_API_USERS = `${API_BASE_URL}/users`;
-const RDS_API_STANDUP = `${API_BASE_URL}/progresses?userId=`;
