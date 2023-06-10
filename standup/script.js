3d6295d92b71824a56e3d9ed628bf6a15e796c25
Shree Ram <sahsisunny@gmail.com>
2023-06-10 06:32:58 +0530

    add error messages

--- standup/script.js
+++ standup/script.js
@@ -1,18 +1,11 @@
-const usersInput = document.getElementById('user-search-input');
+const userInput = document.getElementById('user-search-input');
 const searchButton = document.getElementById('search-button');
 const tableContainer = document.getElementById('table-container');
-let username = [];
 const table = document.createElement('table');
 table.classList.add('user-standup-table');
 const tableBody = document.createElement('tbody');
-async function makeApiCall(
-  url,
-  method = 'get',
-  body = null,
-  credentials = 'include',
-  headers = { 'content-type': 'application/json' },
-  options = null,
-) {
+
+async function makeApiCall(url, method, body, credentials, headers, options) {
   try {
     const response = await fetch(url, {
       method,
@@ -27,22 +20,36 @@ async function makeApiCall(
     throw err;
   }
 }
-const getUserData = async (username) => {
+
+async function getUserData(username) {
   const response = await makeApiCall(`${RDS_API_USERS}/${username}`);
   const data = await response.json();
   return data.user;
-};
-const getStandupData = async (userId) => {
+}
+
+async function getStandupData(userId) {
   const response = await makeApiCall(`${RDS_API_STANDUP}${userId}`);
   const data = await response.json();
   return data.data;
-};
+}
+
 function getStatusArray(apiResponse) {
+  if (!Array.isArray(apiResponse) || apiResponse.length === 0) {
+    const currentDate = new Date();
+    const daysInMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      0,
+    ).getDate();
+    return Array(daysInMonth).fill('❌');
+  }
+
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth();
   const currentYear = currentDate.getFullYear();
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const statusArray = [];
+
   for (let day = 1; day <= daysInMonth; day++) {
     const filteredData = apiResponse.filter((item) => {
       const itemDate = new Date(item.createdAt);
@@ -54,79 +61,132 @@ function getStatusArray(apiResponse) {
         itemYear === currentYear
       );
     });
+
     if (filteredData.length > 0 && day === 1) {
       statusArray.push('✅');
     } else {
-      statusArray.push('❌');
+      statusArray.push(filteredData.length > 0 ? '✅' : '❌');
     }
   }
+
   return statusArray;
 }
-const renderTableHeader = () => {
-  let thead = document.createElement('thead');
-  thead.className = 'table-header';
-  let tr = document.createElement('tr');
-  tr.className = 'table-header';
-  let th = document.createElement('th');
-  th.className = 'user date table-head';
-  th.innerHTML = 'DATES ➡️<hr />USERS ⬇️';
-  tr.appendChild(th);
-  let currentDate = new Date();
-  let currentMonth = currentDate.toLocaleString('default', { month: 'long' });
-  let currentYear = currentDate.getFullYear();
-  let daysInMonth = new Date(
+
+function renderTableHeader() {
+  const tableHeader = document.createElement('thead');
+  tableHeader.className = 'table-header';
+
+  const headerRow = document.createElement('tr');
+  headerRow.className = 'table-header';
+
+  const headerCell = document.createElement('th');
+  headerCell.className = 'user date table-head';
+  headerCell.innerHTML = 'DATES ➡️<hr />USERS ⬇️';
+  headerRow.appendChild(headerCell);
+
+  const currentDate = new Date();
+  const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+  const currentYear = currentDate.getFullYear();
+  const daysInMonth = new Date(
     currentYear,
     currentDate.getMonth() + 1,
     0,
   ).getDate();
+
   for (let day = 1; day <= daysInMonth; day++) {
-    let dateCell = document.createElement('th');
+    const dateCell = document.createElement('th');
     dateCell.scope = 'col';
     dateCell.className = 'date';
-    dateCell.innerHTML = day + ' ' + currentMonth + ' ' + currentYear;
-    tr.appendChild(dateCell);
+    dateCell.textContent = day + ' ' + currentMonth + ' ' + currentYear;
+    headerRow.appendChild(dateCell);
   }
-  thead.appendChild(tr);
-  return thead;
-};
-const tableHeader = renderTableHeader();
-table.appendChild(tableHeader);
-tableContainer.appendChild(table);
-const renderTableBody = (usersName, imageUrl, standupStatus) => {
-  const tr = document.createElement('tr');
-  tr.className = 'table-row';
-  const td = document.createElement('td');
-  td.scope = 'row';
-  td.className = 'user';
-  const div = document.createElement('div');
-  div.className = 'user-list-item';
-  const img = document.createElement('img');
-  img.src = imageUrl;
-  img.alt = usersName;
-  img.className = 'user-image';
-  const p = document.createElement('p');
-  p.className = 'user-name';
-  p.innerHTML = usersName;
-  div.appendChild(img);
-  div.appendChild(p);
-  td.appendChild(div);
-  tr.appendChild(td);
+
+  tableHeader.appendChild(headerRow);
+  return tableHeader;
+}
+
+function renderTableRow({ userName, imageUrl, standupStatus }) {
+  const row = document.createElement('tr');
+  row.className = 'table-row';
+
+  const userCell = document.createElement('td');
+  userCell.scope = 'row';
+  userCell.className = 'user';
+
+  const userContainer = document.createElement('div');
+  userContainer.className = 'user-list-item';
+
+  const userImage = document.createElement('img');
+  userImage.src = imageUrl;
+  userImage.alt = userName;
+  userImage.className = 'user-image';
+
+  const userNameElement = document.createElement('p');
+  userNameElement.className = 'user-name';
+  userNameElement.textContent = userName;
+
+  userContainer.appendChild(userImage);
+  userContainer.appendChild(userNameElement);
+  userCell.appendChild(userContainer);
+  row.appendChild(userCell);
+
   standupStatus.forEach((status) => {
-    const td = document.createElement('td');
-    td.innerHTML = status;
-    tr.appendChild(td);
+    const statusCell = document.createElement('td');
+    statusCell.textContent = status;
+    row.appendChild(statusCell);
   });
-  return tr;
-};
-const searchButtonHandler = async () => {
-  username = usersInput.value.split(',');
-  username.forEach(async (user) => {
-    const userData = await getUserData(user);
-    const standupData = await getStandupData(userData.id);
-    const statusArray = getStatusArray(standupData);
-    const tr = renderTableBody(user, userData?.picture?.url, statusArray);
-    tableBody.appendChild(tr);
+
+  return row;
+}
+
+function initializeTable() {
+  const tableHeader = renderTableHeader();
+  table.appendChild(tableHeader);
+  table.appendChild(tableBody);
+  tableContainer.innerHTML = ''; // Clear the table container first
+  tableContainer.appendChild(table);
+}
+
+async function searchButtonHandler() {
+  const usernames = userInput.value.split(',');
+  tableBody.innerHTML = '';
+
+  if (usernames.length === 0 || usernames[0].trim() === '') {
+    tableContainer.innerHTML = 'Please enter usernames to search.';
+    return;
+  }
+
+  for (const username of usernames) {
+    try {
+      const { id, picture } = await getUserData(username.trim());
+      const standupData = await getStandupData(id);
+      const statusArray = getStatusArray(standupData);
+      const tableRow = renderTableRow({
+        userName: username.trim(),
+        imageUrl: picture?.url,
+        standupStatus: statusArray,
+      });
+      tableBody.appendChild(tableRow);
+    } catch (error) {
+      console.error(`Error fetching data for user: ${username.trim()}`, error);
+    }
+  }
+
+  if (tableBody.firstChild) {
     table.appendChild(tableBody);
-  });
-};
+    tableContainer.innerHTML = '';
+    tableContainer.appendChild(table);
+  } else {
+    tableContainer.innerHTML = 'No matching users found.';
+  }
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    searchButtonHandler();
+  }
+}
+
+initializeTable();
 searchButton.addEventListener('click', searchButtonHandler);
+userInput.addEventListener('keyup', handleKeyPress);
