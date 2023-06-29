ed70f5e2cfc83d647ad6ec7eb84e8f8f3f0201a0
sahsisunny <sahsisunny@gmail.com>
2023-06-30 02:30:36 +0530

    FEAT : add sidebar for standup view

--- standup/utils.js
+++ standup/utils.js
@@ -37,3 +37,44 @@ function createLoaderElement() {
   loaderElement.appendChild(wrapperElement);
   return loaderElement;
 }
+
+function createSidebarPanelElement(
+  completed,
+  planned,
+  blockers,
+  day,
+  currentMonthName,
+  currentYear,
+) {
+  const standupHeadElement = createElement({
+    type: 'h4',
+    classList: ['standup-head'],
+  });
+  standupHeadElement.innerHTML = `Standup for ${day} ${currentMonthName} ${currentYear}`;
+  const sidebarPanelElement = createElement({
+    type: 'div',
+    classList: ['sidebar-panel', 'sidebar'],
+  });
+  sidebarPanelElement.id = 'standupSidebar';
+  const completedElement = createElement({
+    type: 'div',
+    classList: ['completed'],
+  });
+  const plannedElement = createElement({
+    type: 'div',
+    classList: ['planned'],
+  });
+  const blockersElement = createElement({
+    type: 'div',
+    classList: ['blockers'],
+  });
+
+  completedElement.innerHTML = `Yesterday<br>${completed}`;
+  plannedElement.innerHTML = `Today<br>${planned}`;
+  blockersElement.innerHTML = `Blockers<br>${blockers}`;
+  sidebarPanelElement.appendChild(standupHeadElement);
+  sidebarPanelElement.appendChild(completedElement);
+  sidebarPanelElement.appendChild(plannedElement);
+  sidebarPanelElement.appendChild(blockersElement);
+  return sidebarPanelElement;
+}
