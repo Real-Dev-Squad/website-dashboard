01929967c4de2328274f5026920c2c1ae45ea770
sahsisunny <sahsisunny@gmail.com>
2023-07-02 22:24:28 +0530

    FIX : add deleted mock data

---
+++ mock-data/users/index.js
@@ -0,0 +1,137 @@
+const allUsersData = {
+  message: 'Users returned successfully!',
+  users: [
+    {
+      id: 'V4rqL1aDecNGoa1IxiCu',
+      incompleteUserDetails: false,
+      discordId: '12345',
+      roles: {
+        archived: false,
+      },
+      linkedin_id: 'uiram',
+      last_name: 'Raghunathan',
+      yoe: '5',
+      github_display_name: 'Sriram',
+      company_name: 'Juniper networks ',
+      github_id: '19sriram',
+      designation: 'Front end engineer',
+      twitter_id: '19sriram',
+      first_name: 'Sriram',
+      username: '19sriram',
+    },
+    {
+      id: 'oSNvOncbsiMOI5wN4fqV',
+      incompleteUserDetails: false,
+      roles: {
+        archived: false,
+      },
+      last_name: '-',
+      linkedin_id: 'arpit-kumar-4b11211a4',
+      picture: {
+        publicId: 'profile/54vObOfoscwiIVNMSqnN/askdcanhcehukqrdugge',
+        url: 'https://res.cloudinary.com/realdevsquad/image/upload/v1679878917/profile/54vObOfoscwiIVNMSqnN/askdcanhcehukqrdugge.jpg',
+      },
+      yoe: 0,
+      github_display_name: 'Arpit',
+      github_id: 'arpit01923',
+      company:
+        'Seth Jai Prakash Mukand Lal Institute of Engineering and Technology',
+      designation: 'Frontend Developer',
+      twitter_id: 'Arpit_00_02',
+      first_name: 'Arpit',
+      username: 'Arpit_02',
+    },
+    {
+      id: '7yzVDl8s1ORNCtH9Ps7K',
+      profileURL: 'https://profile-service-rds-randhir.herokuapp.com/',
+      website: '',
+      incompleteUserDetails: false,
+      profileStatus: 'BLOCKED',
+      last_name: 'Kumar',
+      linkedin_id: 'randhir-kumar-a4a2981b0',
+      yoe: 4,
+      instagram_id: '',
+      github_display_name: 'Randhir Kumar Singh',
+      github_id: 'heyrandhir',
+      company: 'Autoliv',
+      designation: 'Engineer',
+      twitter_id: 'randhirxp',
+      first_name: 'Randhir',
+      status: 'active',
+      username: 'randhir',
+      picture: {
+        url: 'https://res.cloudinary.com/realdevsquad/image/upload/v1673312957/profile/DtR9sK7CysOVHP17zl8N/bbtkpea622crqotnhsa3.jpg',
+        publicId: 'profile/DtR9sK7CysOVHP17zl8N/bbtkpea622crqotnhsa3',
+      },
+      roles: {
+        archived: false,
+        member: true,
+      },
+    },
+  ],
+};
+
+const filteredUsersData = {
+  message: 'Users returned successfully!',
+  users: [
+    {
+      id: '7yzVDl8s1ORNCtH9Ps7K',
+      profileURL: 'https://profile-service-rds-randhir.herokuapp.com/',
+      website: '',
+      incompleteUserDetails: false,
+      profileStatus: 'BLOCKED',
+      last_name: 'Kumar',
+      linkedin_id: 'randhir-kumar-a4a2981b0',
+      yoe: 4,
+      instagram_id: '',
+      github_display_name: 'Randhir Kumar Singh',
+      github_id: 'heyrandhir',
+      company: 'Autoliv',
+      designation: 'Engineer',
+      twitter_id: 'randhirxp',
+      first_name: 'Randhir',
+      status: 'active',
+      username: 'randhir',
+      picture: {
+        url: 'https://res.cloudinary.com/realdevsquad/image/upload/v1673312957/profile/DtR9sK7CysOVHP17zl8N/bbtkpea622crqotnhsa3.jpg',
+        publicId: 'profile/DtR9sK7CysOVHP17zl8N/bbtkpea622crqotnhsa3',
+      },
+      roles: {
+        archived: false,
+        member: true,
+      },
+    },
+    {
+      id: '2fQEmX2Eo9kya3sGvRL2',
+      github_display_name: 'Bhavika Tibrewal',
+      github_id: 'bhtibrewal',
+      incompleteUserDetails: false,
+      first_name: 'Bhavika',
+      last_name: 'Tibrewal',
+      username: 'bhavika-tibrewal',
+      discordId: '688775365015240740',
+      roles: {
+        archived: false,
+        in_discord: true,
+      },
+      joined_discord: '2023-01-13T18:21:09.278000+00:00',
+      picture: {
+        url: 'https://res.cloudinary.com/realdevsquad/image/upload/v1683338778/profile/sj9t9GBbJ0yvMtw4WXNd/soowyqwenocpit32vqbh.jpg',
+      },
+    },
+    {
+      id: 'bmvrT75ZIwUHu7wr5DUJ',
+      incompleteUserDetails: false,
+      first_name: 'Deipayan',
+      last_name: 'Dash',
+      discordId: '528222297970966539',
+      roles: {
+        archived: false,
+        in_discord: false,
+      },
+      joined_discord: '2023-04-28T12:24:05.465000+00:00',
+    },
+  ],
+};
+
+module.exports = { allUsersData, filteredUsersData };
