966a26a96b13ff55e551f13926fb68a14094e33b
MayurLund <lundmayur07@gmail.com>
2023-07-20 00:54:26 +0530

    add sync buttons

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
 const API_BASE_URL = 'https://api.realdevsquad.com';
 const USER_MANAGEMENT_LINK = 'user-management-link';
 const EXTENSION_REQUESTS_LINK = 'extension-requests-link';
+const SYNC_DISCORD_USER_DATA = 'sync-discord-user-data';
+const SYNC_UNVERIFIED_USER_DATA = 'sync-unverified-user-data';
+const SYNC_DISCORD_USER_DATA_STATUS = 'sync-discord-user-data-status';
+const SYNC_UNVERIFIED_USER_DATA_STATUS = 'sync-unverified-user-data-status';
 const dummyPicture =
   'https://dashboard.realdevsquad.com/users/images/avatar.png';
 const USER_MANAGEMENT_URL =
--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const userManagementLink = document.getElementById(USER_MANAGEMENT_LINK);
 const extensionRequestsLink = document.getElementById(EXTENSION_REQUESTS_LINK);
+const syncDiscordUserDataButton = document.getElementById(
+  SYNC_DISCORD_USER_DATA,
+);
+const syncUnverifiedUserDataButton = document.getElementById(
+  SYNC_UNVERIFIED_USER_DATA,
+);
+const syncDiscordUserDataStatus = document.getElementById(
+  SYNC_DISCORD_USER_DATA_STATUS,
+);
+const syncUnverifiedDiscordStatus = document.getElementById(
+  SYNC_UNVERIFIED_USER_DATA_STATUS,
+);
 
 export async function showSuperUserOptions(...privateBtns) {
   try {
@@ -20,12 +32,94 @@ export async function showSuperUserOptions(...privateBtns) {
  * Then get the node from the DOM into a variable and pass that variable in the
  * function below.
  */
-showSuperUserOptions(userManagementLink, extensionRequestsLink);
+showSuperUserOptions(
+  userManagementLink,
+  extensionRequestsLink,
+  syncDiscordUserDataButton,
+  syncUnverifiedUserDataButton,
+  syncDiscordUserDataStatus,
+  syncUnverifiedDiscordStatus,
+);
 
 const createGoalButton = document.getElementById('create-goal');
 const params = new URLSearchParams(window.location.search);
 if (params.get('dev') === 'true') {
   createGoalButton.classList.remove('element-display-remove');
+}
+
+
+
+syncDiscordUserDataButton.addEventListener('click', syncDiscordUserData);
+syncUnverifiedUserDataButton.addEventListener('click', syncUnverifiedUserData);
+
+
+async function syncDiscordUserData(event) {
+  const button = event.target;
+  const wrapper = button.parentElement;
+  const spinner = wrapper.querySelector('.spinner');
+  const status = wrapper.querySelector('.status');
+
+  button.disabled = true;
+  button.classList.add('disabled');
+  spinner.style.display = 'inline-block';
+  status.textContent = 'Last Sync: In progress';
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/users/status/update`, {
+      method: 'PATCH',
+      credentials: 'include',
+    });
+
+    // Check if the response is ok
+    if (response.ok) {
+      status.textContent = 'Last Sync: Successful';
+    } else {
+      status.textContent = 'Last Sync: Failed';
+    }
+  } catch (err) {
+    console.error(err);
+    status.textContent = 'Last Sync: Failed';
+  } finally {
+    spinner.style.display = 'none';
+    button.classList.remove('disabled');
+    button.disabled = false;
+  }
+}
+
+//for external accounts
+async function syncUnverifiedUserData(event) {
+  const button = event.target;
+  const wrapper = button.parentElement;
+  const spinner = wrapper.querySelector('.spinner');
+  const status = wrapper.querySelector('.status');
+
+  button.disabled = true;
+  button.classList.add('disabled');
+  spinner.style.display = 'inline-block';
+  status.textContent = 'Last Sync: In progress';
+
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/external-accounts/discord-sync`,
+      {
+        method: 'PATCH',
+        credentials: 'include',
+      },
+    );
+    // Check if the response is ok
+    if (response.ok) {
+      status.textContent = 'Last Sync: Successful';
+    } else {
+      status.textContent = 'Last Sync: Failed';
+    }
+  } catch (err) {
+    console.error(err);
+    status.textContent = 'Last Sync: Failed';
+  } finally {
+    spinner.style.display = 'none';
+    button.classList.remove('disabled');
+    button.disabled = false;
+  }
 }
 
 showUserManagementButton();
