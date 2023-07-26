ada3f6b4af1ff2fd3149cf5dd4ea977314455ae8
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-07-26 23:04:12 +0530

    fix rsponse not being read

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function getCurrentTimestamp() {
 
 export async function showSuperUserOptions(...privateBtns) {
   try {
-    const isSuperUser = true; //await checkUserIsSuperUser();
+    await checkUserIsSuperUser();
     if (isSuperUser) {
       privateBtns.forEach((btn) =>
         btn.classList.remove('element-display-remove'),
@@ -127,7 +127,6 @@ async function handleSync(
 }
 
 function showToast(message, type) {
-  console.log(typeof message);
   if (typeof message === 'string') {
     toast.innerHTML = `<div class="message">${message}</div>`;
   }
@@ -171,11 +170,11 @@ const repoSyncHandler = async (event) => {
   status.textContent = SYNC_IN_PROGRESS;
 
   try {
-    const response = await fetch(REPO_SYNC_API_URL, { mode: 'no-cors' });
-    console.log(response);
-    if (response.ok) {
+    const apiResponse = await fetch(REPO_SYNC_API_URL);
+    const response = await apiResponse.json();
+    if (apiResponse.ok) {
       repoSyncStatusUpdate.textContent = SYNC_SUCCESSFUL;
-      showToast(response.body, 'success');
+      showToast(response, 'success');
     } else {
       repoSyncStatusUpdate.textContent = SYNC_FAILED;
       showToast('API response not as expected', 'failure');
