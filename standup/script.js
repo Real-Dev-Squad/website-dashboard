c81810cc0572604e1500fa2a6f3b4e4b532179e8
mailmesriza98 <srizasen54554@gmail.com>
2023-08-13 16:53:48 +0530

    added changes for 3 month data

--- standup/script.js
+++ standup/script.js
@@ -13,9 +13,29 @@ const daysInCurrentMonth = new Date(
   currentDateObj.getMonth() + 1,
   0,
 ).getDate();
-const currentMonthName = currentDateObj.toLocaleString('default', {
-  month: 'long',
-});
+
+function isSunday(date) {
+  return date.getDay() === 0; // 0 represents Sunday
+}
+const currentMonthNum = currentDateObj.getMonth();
+
+const endDate = currentDateObj;
+const startDate = new Date(currentYearNum, currentMonthNum - 3, 1); // Start from 3 months ago
+
+function formatDateFromTimestamp(timestamp) {
+  const dateObject = new Date(timestamp);
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1;
+  const day = dateObject.getDate();
+  return `${day}-${month}-${year}`;
+}
+
+function formatDate(dateObject) {
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1;
+  const day = dateObject.getDate();
+  return `${day}-${month}-${year}`;
+}
 
 async function fetchUserData(username) {
   const response = await makeApiCall(`${RDS_API_USERS}/${username}`);
@@ -28,39 +48,75 @@ async function fetchStandupData(userId) {
   const data = await response.json();
   return data.data;
 }
-
+const oneDay = 24 * 60 * 60 * 1000;
 function processStandupData(standupItems) {
   const standupData = {
     standupFrequency: [],
     completedText: [],
     plannedText: [],
     blockersText: [],
+    date: [],
+    month: [],
+    year: [],
   };
-  standupData.standupFrequency = Array(daysInCurrentMonth).fill('❌');
-  standupData.completedText = Array(daysInCurrentMonth).fill('No data');
-  standupData.plannedText = Array(daysInCurrentMonth).fill('No data');
-  standupData.blockersText = Array(daysInCurrentMonth).fill('No data');
 
   if (!standupItems) {
+    for (
+      let date = new Date(endDate);
+      date >= startDate;
+      date = new Date(date.getTime() - oneDay)
+    ) {
+      if (!isSunday(date)) {
+        standupData.standupFrequency.push('❌');
+        standupData.completedText.push('No data');
+        standupData.plannedText.push('No data');
+        standupData.blockersText.push('No data');
+        //${date.getDate()} ${date.toLocaleString('default', {month: 'long',})} ${date.getFullYear()}
+        standupData.date.push(date.getDate());
+        standupData.month.push(
+          date.toLocaleString('default', { month: 'long' }),
+        );
+        standupData.year.push(date.getFullYear());
+      }
+    }
     return standupData;
   }
+
+  const standupDataByDate = {};
   for (let i = 0; i < standupItems.length; i++) {
-    const standupItem = standupItems[i];
-    const date = new Date(standupItem.createdAt);
-    const day = date.getDate();
-    const month = date.getMonth();
-    const index = day - 1;
+    const formattedDate = formatDateFromTimestamp(standupItems[i].createdAt);
+    standupDataByDate[formattedDate] = standupItems[i];
+  }
 
-    const isCurrentMonth = month === currentDateObj.getMonth();
-    const isValidIndex =
-      index >= 0 && index < standupData.standupFrequency.length;
-
-    if (isCurrentMonth && isValidIndex) {
-      const { completed, planned, blockers } = standupItem;
-      standupData.standupFrequency[index] = '✅';
-      standupData.completedText[index] = completed;
-      standupData.plannedText[index] = planned;
-      standupData.blockersText[index] = blockers;
+  for (
+    let date = new Date(endDate);
+    date >= startDate;
+    date = new Date(date.getTime() - oneDay)
+  ) {
+    if (!isSunday(date)) {
+      if (standupDataByDate[formatDate(date)]) {
+        const { completed, planned, blockers } =
+          standupDataByDate[formatDate(date)];
+        standupData.standupFrequency.push('✅');
+        standupData.completedText.push(completed);
+        standupData.plannedText.push(planned);
+        standupData.blockersText.push(blockers);
+        standupData.date.push(date.getDate());
+        standupData.month.push(
+          date.toLocaleString('default', { month: 'long' }),
+        );
+        standupData.year.push(date.getFullYear());
+      } else {
+        standupData.standupFrequency.push('❌');
+        standupData.completedText.push('No data');
+        standupData.plannedText.push('No data');
+        standupData.blockersText.push('No data');
+        standupData.date.push(date.getDate());
+        standupData.month.push(
+          date.toLocaleString('default', { month: 'long' }),
+        );
+        standupData.year.push(date.getFullYear());
+      }
     }
   }
   return standupData;
@@ -81,15 +137,23 @@ function createTableHeaderElement() {
   });
   headerCellElement.innerHTML = 'DATES ➡️<hr />USERS ⬇️';
   headerRowElement.appendChild(headerCellElement);
-  for (let day = 1; day <= daysInCurrentMonth; day++) {
-    const dateCellElement = createElement({
-      type: 'th',
-      classList: ['date'],
-      scope: 'row',
-    });
-    dateCellElement.textContent =
-      day + ' ' + currentMonthName + ' ' + currentYearNum;
-    headerRowElement.appendChild(dateCellElement);
+  for (
+    let date = new Date(endDate);
+    date >= startDate;
+    date = new Date(date.getTime() - oneDay)
+  ) {
+    if (!isSunday(date)) {
+      const dateCellElement = createElement({
+        type: 'th',
+        classList: ['date'],
+        scope: 'row',
+      });
+      dateCellElement.textContent = `${date.getDate()} ${date.toLocaleString(
+        'default',
+        { month: 'long' },
+      )} ${date.getFullYear()}`;
+      headerRowElement.appendChild(dateCellElement);
+    }
   }
   tableHeaderElement.appendChild(headerRowElement);
   return tableHeaderElement;
@@ -119,13 +183,14 @@ function createTableRowElement({ userName, imageUrl, userStandupData }) {
   userContainerElement.appendChild(userNameElement);
   userCellElement.appendChild(userContainerElement);
   rowElement.appendChild(userCellElement);
-
   const standupStatus = userStandupData.standupFrequency;
   const completedTextData = userStandupData.completedText;
   const plannedText = userStandupData.plannedText;
   const blockersText = userStandupData.blockersText;
-
-  for (let i = 0; i < daysInCurrentMonth; i++) {
+  const date = userStandupData.date;
+  const month = userStandupData.month;
+  const year = userStandupData.year;
+  for (let i = 0; i < standupStatus.length; i++) {
     const statusCellElement = createElement({
       type: 'td',
       classList: ['status'],
@@ -158,9 +223,9 @@ function createTableRowElement({ userName, imageUrl, userStandupData }) {
       completedTextData[i],
       plannedText[i],
       blockersText[i],
-      i + 1,
-      currentMonthName,
-      currentYearNum,
+      date[i],
+      month[i],
+      year[i],
     );
 
     if (standupStatus[i] === '✅') {
