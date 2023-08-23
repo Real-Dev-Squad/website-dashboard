403abe3a2bd1b966cfe9992b5445aac4bbde3c6a
meanwhile7 <abhijit793001@gmail.com>
2023-08-23 22:26:17 +0530

    test for Intro button

---
+++ __tests__/user-details/Intro-button.test.js
@@ -0,0 +1,77 @@
+const puppeteer = require('puppeteer');
+const { userDetails } = require('../../mock-data/user-details/index');
+
+describe('Intro User Button Users Detail Page', () => {
+  let browser;
+  let page;
+
+  jest.setTimeout(60000);
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: 'new',
+      ignoreHTTPSErrors: true,
+      args: ['--incognito', '--disable-web-security'],
+      devtools: false,
+    });
+    page = await browser.newPage();
+
+    await page.setRequestInterception(true);
+
+    page.on('request', (interceptedRequest) => {
+      const url = interceptedRequest.url();
+      if (url === 'https://api.realdevsquad.com/users/randhir') {
+        interceptedRequest.respond({
+          status: 200,
+          contentType: 'application/json',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+          body: JSON.stringify(userDetails),
+        });
+      } else {
+        interceptedRequest.continue();
+      }
+    });
+    await page.goto(
+      'http://localhost:5500/users/details/index.html?username=randhir',
+    );
+    await page.waitForNetworkIdle();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it('should render the button', async () => {
+    const pivotButton = await page.$('.Intro');
+    expect(pivotButton).toBeTruthy();
+  });
+
+  it('should show tooltip on button hover for non-superusers', async () => {
+    
+    const button = await page.$('.disabled');
+    expect(button).toBeTruthy();
+
+    await button.hover();
+    await page.waitForTimeout(500);
+
+    const tooltipElement = await page.$('.tooltip');
+    expect(tooltipElement).toBeTruthy();
+
+    const tooltipText = await tooltipElement.evaluate(
+      (tooltip) => tooltip.textContent,
+    );
+    expect(tooltipText).toContain(
+      'You do not have required permissions to view this.',
+    );
+
+    await page.mouse.move(0, 0);
+    await page.waitForTimeout(500);
+
+    const updatedTooltipElement = await page.$('.tooltip');
+    expect(updatedTooltipElement).toBeFalsy();
+  });
+});
---
+++ mock-data/user-details/index.js
@@ -0,0 +1,36 @@
+const userDetails = {
+  message: 'User returned successfully!',
+  user: {
+    id: 'DtR9sK7CysOVHP17zl8N',
+    profileURL: 'https://profile-service-rds-randhir.herokuapp.com/',
+    discordJoinedAt: '2022-01-02T02:56:16.935000+00:00',
+    roles: {
+      archived: false,
+      in_discord: true,
+      member: true,
+    },
+    profileStatus: 'BLOCKED',
+    yoe: 4,
+    updated_at: 1691884285042,
+    company: 'Autoliv',
+    twitter_id: 'randhirxp',
+    first_name: 'Randhir',
+    website: '',
+    incompleteUserDetails: false,
+    discordId: '806833869038944276',
+    last_name: 'Kumar',
+    linkedin_id: 'randhir-kumar-a4a2981b0',
+    picture: {
+      url: 'https://res.cloudinary.com/realdevsquad/image/upload/v1673312957/profile/DtR9sK7CysOVHP17zl8N/bbtkpea622crqotnhsa3.jpg',
+      publicId: 'profile/DtR9sK7CysOVHP17zl8N/bbtkpea622crqotnhsa3',
+    },
+    instagram_id: '',
+    github_display_name: 'Randhir Kumar Singh',
+    github_id: 'heyrandhir',
+    designation: 'Engineer',
+    status: 'active',
+    username: 'randhir',
+  },
+};
+
+module.exports = { userDetails};
