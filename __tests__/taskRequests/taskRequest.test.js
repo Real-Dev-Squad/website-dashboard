22735a11bd72baf645d5176edf660771e56f018a
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2023-08-29 00:22:11 +0530

    solved testing issues

--- __tests__/taskRequests/taskRequest.test.js
+++ __tests__/taskRequests/taskRequest.test.js
@@ -9,7 +9,6 @@ const API_BASE_URL = 'https://staging-api.realdevsquad.com';
 describe('Task Requests', () => {
   let browser;
   let page;
-  jest.setTimeout(9000);
 
   beforeAll(async () => {
     browser = await puppeteer.launch();
