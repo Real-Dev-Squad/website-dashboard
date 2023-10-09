8de36dd9312549980eff7206b8f4d785ea66f828
ishant005 <91171097+ishant005@users.noreply.github.com>
2023-10-09 21:02:28 +0530

    Fix Profile Picture Display for Users without Profile Pictures #532 (#542)
    added dummy picture
    
    Co-authored-by: Ajeyakrishna <98796547+Ajeyakrishna-k@users.noreply.github.com>

--- constants.js
+++ constants.js
@@ -27,7 +27,6 @@ const STATUS_BASE_URL_PROD = 'https://status.realdevsquad.com';
 const STATUS_BASE_URL_STAGING = 'https://staging-status.realdevsquad.com';
 const STATUS_BASE_URL = STATUS_BASE_URL_PROD;
 
-const dummyPicture =
-  'https://dashboard.realdevsquad.com/users/images/avatar.png';
+const dummyPicture = 'https://dashboard.realdevsquad.com/images/avatar.png';
 const USER_MANAGEMENT_URL =
   'https://dashboard.realdevsquad.com/users/details/?username=';
