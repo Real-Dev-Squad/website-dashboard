7c0136fdf9c236dda308dd9e3d30518194ab755c
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-11-04 18:29:59 +0530

    resolve comments from Bhavika

--- taskRequests/details/script.js
+++ taskRequests/details/script.js
@@ -334,20 +334,6 @@ window.addEventListener('click', function (event) {
   }
 });
 
-function getHumanReadableDate(timeStamp) {
-  if (typeof timeStamp !== 'number') {
-    return 'N/A';
-  }
-  const date = new Date(timeStamp * 1000);
-
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  const formattedDate = `${day}-${month}-${year}`;
-  return formattedDate;
-}
-
 function populateModalContent(index) {
   if (
     !Array.isArray(taskRequest.users) ||
