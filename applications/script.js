53fdad24f11e91a6b41e86c7658a619e4ec0813b
vinayak <trivedivinayak57@gmail.com>
2023-12-15 05:59:35 +0530

    intial API integration for applications

--- applications/script.js
+++ applications/script.js
@@ -1,4 +1,6 @@
-import { createElement } from './utils.js';
+import { createElement, getApplications } from './utils.js';
+const BASE_URL = 'https://localhost:3000';
+let status = 'all';
 
 function createApplicationCard({ username, companyName, skills, intro }) {
   const applicationCard = createElement({
@@ -52,16 +54,54 @@ function createApplicationCard({ username, companyName, skills, intro }) {
   return applicationCard;
 }
 
-(function renderApplicationCards() {
+async function renderApplicationCards() {
   const applicationContainer = document.querySelector('.application-container');
-  Array.from({ length: 10 }).map(() => {
+  const { applications } = await getApplications({ applicationStatus: 'all' });
+  console.log(applications, 'applications');
+  applications.forEach((application) => {
     const applicationCard = createApplicationCard({
-      username: 'Vinayak',
-      companyName: 'JumpingMinds',
-      intro:
-        'mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pe..',
-      skills: 'React, Node js',
+      username: application.biodata.firstName,
+      companyName: application.professional.institution,
+      intro: `${application.intro.introduction.slice(0, 200)}...`,
+      skills: application.professional.skills,
     });
     applicationContainer.appendChild(applicationCard);
   });
-})();
+}
+
+renderApplicationCards();
+
+const data = {
+  id: 'bWkwmQRGbdZIVV6qiXG2',
+  createdAt: {
+    _seconds: 1702598350,
+    _nanoseconds: 357000000,
+  },
+  intro: {
+    funFact:
+      'mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at',
+    forFun:
+      'mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at',
+    numberOfHours: 14,
+    whyRds:
+      'mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at',
+    introduction:
+      'mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at',
+  },
+  biodata: {
+    firstName: 'vinayak',
+    lastName: 'trivedi',
+  },
+  location: {
+    country: 'India',
+    city: 'Kanpur',
+    state: 'UP',
+  },
+  foundFrom: 'twitter',
+  userId: 'hKzs2IQGe4sLnAuSZ85i',
+  professional: {
+    skills: 'REACT, NODE JS',
+    institution: 'Christ church college',
+  },
+  status: 'pending',
+};
--- applications/utils.js
+++ applications/utils.js
@@ -1,3 +1,5 @@
+const BASE_URL = 'http://localhost:3000';
+
 function createElement({ type, attributes = {}, innerText }) {
   const element = document.createElement(type);
   Object.keys(attributes).forEach((item) => {
@@ -7,4 +9,24 @@ function createElement({ type, attributes = {}, innerText }) {
   return element;
 }
 
-export { createElement };
+async function getApplications({ applicationStatus, size = 5, next = '' }) {
+  let url;
+
+  if (applicationStatus === 'all') {
+    url = `${BASE_URL}/applications?size=${size}`;
+  } else {
+    url = `${BASE_URL}/applications?size=${size}&status=${applicationStatus}`;
+  }
+
+  const res = await fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  const data = res.json();
+  return data;
+}
+
+export { createElement, getApplications };
