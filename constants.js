473b45d285b7a6791f6ebb7265d53181e108647f
pavangbhat <pavanbhat17@gmail.com>
2024-01-23 22:26:42 +0530

    remove all the changes to find root cause of test failure on CI

--- constants.js
+++ constants.js
@@ -1,17 +1,4 @@
-let API_BASE_URL;
-
-/* 
-During test script we would not have access to winodw, therefore we will set 
-API_BASE_URL to prod API
-*/
-try {
-  if (typeof window !== 'undefined') {
-    API_BASE_URL = window.API_BASE_URL;
-  }
-} catch (err) {
-  API_BASE_URL = 'https://api.realdevsquad.com';
-}
-
+const API_BASE_URL = 'https://api.realdevsquad.com';
 const REPO_SYNC_API_URL =
   'https://staging-sync.staging-realdevsquad-com.workers.dev';
 const USER_MANAGEMENT_LINK = 'user-management-link';
