bbe0bec7971dfa6409d28f361906533dc6287250
pavangbhat <pavanbhat17@gmail.com>
2024-01-29 09:13:15 +0530

    improved readability of loadEnv.js

--- helpers/loadENV.js
+++ helpers/loadENV.js
@@ -1,6 +1,8 @@
-window.API_BASE_URL =
-  window.location.hostname !== 'dashboard.realdevsquad.com'
-    ? window.location.hostname !== 'localhost'
-      ? 'https://staging-api.realdevsquad.com'
-      : 'https://api.realdevsquad.com'
-    : 'https://api.realdevsquad.com';
+window.API_BASE_URL = 'https://api.realdevsquad.com';
+
+if (
+  window.location.hostname !== 'dashboard.realdevsquad.com' &&
+  window.location.hostname !== 'localhost'
+) {
+  window.API_BASE_URL = 'https://staging-api.realdevsquad.com';
+}
