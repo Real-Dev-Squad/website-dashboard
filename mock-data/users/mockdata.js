c7c0e548c184a7e91e7b271d6b3bbcd6cf527312
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-02-09 02:12:55 +0530

    add: test cases add for apply filter pagination

---
+++ mock-data/users/mockdata.js
@@ -0,0 +1,37 @@
+const mockUserData = {
+  message: 'Users found successfully!',
+  users: [
+    {
+      id: 'aaL1MXrpmnUNfLkhgXRj',
+      github_created_at: 1595870020000,
+      github_display_name: 'Shubham Sharma',
+      github_id: 'skv93-coder',
+      incompleteUserDetails: false,
+      discordId: '1005550883062415400',
+      last_name: 'Sharma',
+      linkedin_id:
+        'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHYMsRP3tc0OAAAAYoNZX6wuATNqBsHaNmcvvyvI7xW6_p1BWwaPmUuzm_BCNN9-yOKsgGnYm0D8lgJIw3wn_5LghX6G6_oytuczTfM5P6SsJRZy7LFYiEoIs8YPP8Bx5IkPls=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fshubham-sharma-165600206',
+      company: 'Igzy',
+      designation: 'Junior engineer',
+      twitter_id: 'ShubhamSha11638',
+      first_name: 'Shubham',
+      username: 'shubham-sharma',
+      created_at: 1705233567138,
+      github_user_id: '68867418',
+      updated_at: 1707409606780,
+      roles: {
+        member: false,
+        in_discord: true,
+        archived: false,
+        super_user: true,
+      },
+    },
+  ],
+  links: {
+    next: '/search?1=10&state=ACTIVE,OOO,IDLE,ONBOARDING,ONBOARDING&time=31d&size=10&dev=true',
+    prev: null,
+  },
+  count: 39,
+};
+
+module.exports = { mockUserData };
