838b9566c789bb78fb74a11d1a10dfd84a6a5e20
Swapnil Gawande <88383863+Swapnil099@users.noreply.github.com>
2024-02-27 15:35:16 +0530

    feature: request management system page (#701)

---
+++ requests/util.js
@@ -0,0 +1,88 @@
+function createElementFromMap(domObjectMap) {
+  if (
+    !domObjectMap ||
+    typeof domObjectMap !== 'object' ||
+    !domObjectMap.tagName
+  ) {
+    throw new Error('Invalid domObjectMap. tagName is required.');
+  }
+  const el = document.createElement(domObjectMap.tagName);
+  for (const [key, value] of Object.entries(domObjectMap)) {
+    if (key === 'tagName') {
+      continue;
+    }
+    if (key === 'eventListeners') {
+      value.forEach((obj) => {
+        el.addEventListener(obj.event, obj.func);
+      });
+    }
+    if (key === 'class') {
+      if (Array.isArray(value)) {
+        el.classList.add(...value);
+      } else {
+        el.classList.add(value);
+      }
+    } else if (key === 'child') {
+      el.append(...value);
+    } else {
+      el[key] = value;
+    }
+  }
+
+  return el;
+}
+
+function getOooQueryParamsString(query) {
+  let queryParam = 'type=OOO';
+  if (
+    query.state !== undefined &&
+    query.state !== null &&
+    query.state !== 'ALL'
+  ) {
+    queryParam += `&state=${query.state}`;
+  }
+  return `?${queryParam}`;
+}
+
+function convertDateToReadableStringDate(date, format) {
+  if (format === undefined || format === null) {
+    format = DEFAULT_DATE_FORMAT;
+  }
+  if (date !== undefined && date !== null) {
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    return new Date(date)
+      .toLocaleString('default', options)
+      .replace('DD', new Date(date).getDate())
+      .replace(
+        'MMM',
+        new Date(date).toLocaleString('default', { month: 'short' }),
+      )
+      .replace('YYYY', new Date(date).getFullYear());
+  }
+  return 'N/A';
+}
+
+function getFullNameOfUser(user) {
+  if (!user || typeof user !== 'object') {
+    return 'N/A';
+  }
+  const firstName = user.first_name || 'N/A';
+  const lastName = user.last_name || '';
+  return (
+    firstName.charAt(0).toUpperCase() +
+    firstName.slice(1).toLowerCase() +
+    ' ' +
+    lastName.charAt(0).toUpperCase() +
+    lastName.slice(1).toLowerCase()
+  );
+}
+
+function extractQueryParameters(url) {
+  const searchParams = new URLSearchParams(url.split('?')[1]);
+  const parameters = {};
+
+  for (const [key, value] of searchParams.entries()) {
+    parameters[key] = value;
+  }
+  return parameters;
+}
