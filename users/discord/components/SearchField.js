799e7fb2d5e546e13608efc281c8c82d69da5d24
theOGCodeWitcher <rahulsingh97r@gmail.com>
2024-03-14 01:01:57 -0400

    prettier formatting issue fixed

--- users/discord/components/SearchField.js
+++ users/discord/components/SearchField.js
@@ -1,15 +1,20 @@
 const { createElement } = react;
 
-export const SearchField = ({ onSearchChange }) => createElement('section', {class :'search_section'},
-  [createElement('input', {
-    class: 'search_field',
-    type: 'text',
-    placeholder: 'Search users...',
-  }),
-  createElement('button',  {
-    class :'search_button',
-    onclick: ()=>{onSearchChange(document.querySelector('.search_field').value)},
-  }, ['Search'] )]
-);
-
-
+export const SearchField = ({ onSearchChange }) =>
+  createElement('section', { class: 'search_section' }, [
+    createElement('input', {
+      class: 'search_field',
+      type: 'text',
+      placeholder: 'Search users...',
+    }),
+    createElement(
+      'button',
+      {
+        class: 'search_button',
+        onclick: () => {
+          onSearchChange(document.querySelector('.search_field').value);
+        },
+      },
+      ['Search'],
+    ),
+  ]);
