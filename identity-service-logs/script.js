0432550aafe60bc6a1dd60a09126d09bd9d3190b
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-08-06 03:39:56 +0530

    Page for Identity Service Logs

---
+++ identity-service-logs/script.js
@@ -0,0 +1,36 @@
+import { SUPER_USER } from './constants.js';
+
+import {
+  getIdentityLogs,
+  getSelfUser,
+  fillData,
+  getUserCount,
+} from './utils.js';
+
+const self_user = await getSelfUser();
+
+if (self_user?.roles[SUPER_USER]) {
+  const {
+    verifiedUsersCount,
+    blockedUsersCount,
+    verifiedDeveloperCount,
+    blockedDeveloperCount,
+    developersLeftToVerifyCount,
+    developersCount,
+  } = await getUserCount();
+  document.getElementById('verified').innerText = verifiedUsersCount;
+  document.getElementById('blocked').innerText = blockedUsersCount;
+  document.getElementById('developers').innerText = developersCount;
+  document.getElementById('verifiedDevelopers').innerText =
+    verifiedDeveloperCount;
+  document.getElementById('blockedDevelopers').innerText =
+    blockedDeveloperCount;
+  document.getElementById('developersLeft').innerText =
+    developersLeftToVerifyCount;
+  const { identityLogs, next, prev } = await getIdentityLogs(
+    '/logs?dev=true&type=PROFILE_BLOCKED,PROFILE_VERIFIED,PROFILE_DIFF_REJECTED,PROFILE_DIFF_APPROVED,PROFILE_DIFF_STORED&size=10',
+  );
+  fillData(identityLogs, next, prev);
+} else {
+  document.getElementById('loader').innerHTML = 'You are not authorized !';
+}
