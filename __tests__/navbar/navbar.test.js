5bdead1bc2458ffad32fe389964ceac269c7d48a
Sourav Mehra <36883648+mehra-sourav@users.noreply.github.com>
2024-08-17 11:59:52 +0530

    Navbar globalization (#768)
    * feat: added script to add navbar to HTML pages
    
    * refactor: Seperate user login logic from script.js
    
    The user login functionality has been moved to userLogin.js to allow
    its reuse in all HTML files that would contain the navbar.
    
    * test: add navbar test
    
    * test: update failing test because of earlier code refactor
    
    * feat: add navbar through script to task page and corresponding test
    
    * feat: add navbar through script to profile page and corresponding test
    
    * feat: add navbar through script to Discord users page with corresponding test
    
    * feat: add navbar through script to User Managment page with corresponding test
    
    * test: update failing test
    
    * test: update failing test
    
    * feat: add navbar through script to Extension Requests page with corresponding test
    
    * test: update failing test for extension-requests
    
    * feat: add navbar through script to Task Requests page with corresponding test
    
    * feat: add navbar through script to Online Members page with corresponding test
    
    * feat: add navbar through script to Standup Updated page with corresponding test
    
    * fix(test): fix task requests test by closing browser after each test suite
    
    * feat: add navbar through script to Identity Service Logs page with corresponding test
    
    * feat: add navbar through script to Requests page with corresponding test
    
    * feat: add navbar through script to Activity Feed page with corresponding test
    
    * perf: close page after each test in navbar test to improve Puppeteer test suite speed
    
    * test: update failing tests
    
    * refactor: add window events to trigger navbar initialization

---
+++ __tests__/navbar/navbar.test.js
@@ -0,0 +1,100 @@
+const puppeteer = require('puppeteer');
+
+const baseUrl = 'http://localhost:8000';
+describe('Tests the navbar and its components on various pages', () => {
+  let browser;
+  let page;
+  jest.setTimeout(60000);
+
+  const testNavbar = async (navbarPage) => {
+    const navbar = await navbarPage.$('#tasksNav');
+    expect(navbar).toBeTruthy();
+
+    const rdsLogo = await navbarPage.$('.logo');
+    expect(rdsLogo).toBeTruthy();
+
+    const navLinks = await navbarPage.$('.nav-links');
+    expect(navLinks).toBeTruthy();
+  };
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: 'new',
+      ignoreHTTPSErrors: true,
+      args: ['--incognito', '--disable-web-security'],
+      devtools: false,
+    });
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it('Renders the navbar correctly on the home page', async () => {
+    await page.goto(`${baseUrl}`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the tasks page', async () => {
+    await page.goto(`${baseUrl}/task`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the profile page', async () => {
+    await page.goto(`${baseUrl}/profile`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Discord users page', async () => {
+    await page.goto(`${baseUrl}/users/discord`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the User Management page', async () => {
+    await page.goto(`${baseUrl}/users`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Extension Requests page', async () => {
+    await page.goto(`${baseUrl}/extension-requests`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Task Requests page', async () => {
+    await page.goto(`${baseUrl}/task-requests`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Online Members page', async () => {
+    await page.goto(`${baseUrl}/online-members/online-members.html`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Standup Updates page', async () => {
+    await page.goto(`${baseUrl}/standup/index.html`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Identity Service Logs page', async () => {
+    await page.goto(`${baseUrl}/identity-service-logs/index.html`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Requests page', async () => {
+    await page.goto(`${baseUrl}/requests/index.html`);
+    await testNavbar(page);
+  });
+
+  it('Renders the navbar correctly on the Activity Feed page', async () => {
+    await page.goto(`${baseUrl}/feed/index.html`);
+    await testNavbar(page);
+  });
+});
