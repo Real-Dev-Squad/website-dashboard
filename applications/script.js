1563095e50bc42333142115fb276af71f72a47e0
ahsrah7 <harsha.wardan7@gmail.com>
2024-09-23 15:55:04 +0530

    Display applied filters on applications page with clear option

--- applications/script.js
+++ applications/script.js
@@ -480,9 +480,11 @@ if (isDev) {
 }
 
 function showAppliedFilter(filterApplied) {
-  filterLabel.classList.remove('hidden');
-  filterText.textContent =
-    'Status :' + filterApplied[0].toUpperCase() + filterApplied.substring(1);
+  if(filterApplied){
+    filterLabel.classList.remove('hidden');
+    filterText.textContent =
+      'Status :' + filterApplied[0].toUpperCase() + filterApplied.substring(1);
+  } 
 }
 
 function applyFilter(filter) {
