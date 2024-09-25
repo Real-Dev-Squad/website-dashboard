1bbfce5c179e6b4c6195e53b589be361d6dda97f
Ishan Veer <ishanveer@Ishans-MacBook-Air.local>
2024-09-25 10:14:16 +0530

    added function to initially load 20 users and then load more users as we scroll, changed data fetching structure to fetch last user id

--- users/discord/components/UsersSection.js
+++ users/discord/components/UsersSection.js
@@ -1,26 +1,39 @@
 const { createElement } = react;
 
-export const UsersSection = ({ users, showUser, handleUserSelected }) => {
+export const UsersSection = ({
+  users,
+  showUser,
+  handleUserSelected,
+  handleScroll,
+  isLoading,
+}) => {
   return createElement(
     'aside',
-    { class: 'users_section', onclick: handleUserSelected },
-    users?.map((user) => {
-      return createElement(
-        'div',
-        {
-          class: `user_card ${
-            users[showUser].id === user.id ? 'active_tab' : ''
-          }`,
-          data_key: user.id,
-        },
-        [
-          createElement('img', {
-            src: user?.picture?.url ?? dummyPicture,
-            class: 'user_image',
-          }),
-          createElement('span', {}, [user.first_name + ' ' + user.last_name]),
-        ],
-      );
-    }),
+    {
+      class: 'users_section',
+      onclick: handleUserSelected,
+      onscroll: handleScroll,
+    },
+    [
+      ...users?.map((user) => {
+        return createElement(
+          'div',
+          {
+            class: `user_card ${
+              users[showUser].id === user.id ? 'active_tab' : ''
+            }`,
+            data_key: user.id,
+          },
+          [
+            createElement('img', {
+              src: user?.picture?.url ?? dummyPicture,
+              class: 'user_image',
+            }),
+            createElement('span', {}, [user.first_name + ' ' + user.last_name]),
+          ],
+        );
+      }),
+      isLoading && createElement('div', 'Load more users...'),
+    ],
   );
 };
--- users/discord/utils/util.js
+++ users/discord/utils/util.js
@@ -1,8 +1,12 @@
-export const getUsers = async (tab) => {
+export const getUsers = async (tab, lastUserId = null, limit = 20) => {
   let URL = {
     in_discord: `${API_BASE_URL}/users/search/?role=in_discord`,
     verified: `${API_BASE_URL}/users/search/?verified=true`,
   };
+  if (lastUserId) {
+    URL[tab] += `&last_id=${lastUserId}`;
+  }
+  URL[tab] += `&limit=${limit}`;
 
   try {
     const response = await fetch(URL[tab], {
@@ -12,6 +16,9 @@ export const getUsers = async (tab) => {
         'Content-type': 'application/json',
       },
     });
+    if (!response.ok) {
+      console.error('Failed to fetch:', response.status, response.statusText);
+    }
 
     const data = await response.json();
     return data.users ?? [];
