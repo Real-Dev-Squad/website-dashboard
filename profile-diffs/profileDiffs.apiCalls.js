8a571eea5ffa616d1ced7684250d151e11700f6a
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-09-27 03:52:02 +0530

    Refactor filenames

--- profile-diffs/profileDiffs.apiCalls.js
+++ profile-diffs/profileDiffs.apiCalls.js
@@ -1,23 +1,3 @@
-// Utility functions
-const parseProfileDiffParams = (uri, nextPageParamsObject) => {
-  const urlSearchParams = new URLSearchParams(uri);
-  for (const [key, value] of urlSearchParams.entries()) {
-    nextPageParamsObject[key] = value;
-  }
-  return nextPageParamsObject;
-};
-
-const generateProfileDiffsParams = (nextPageParams, forApi = true) => {
-  const urlSearchParams = new URLSearchParams();
-  for (const [key, value] of Object.entries(nextPageParams)) {
-    if (!value) continue;
-    urlSearchParams.append(key, value);
-  }
-  return `/${
-    forApi ? 'profileDiffs' : 'profile-diffs'
-  }?${urlSearchParams.toString()}`;
-};
-
 const getProfileDiffs = async (query = {}, nextLink) => {
   const finalUrl =
     API_BASE_URL +
