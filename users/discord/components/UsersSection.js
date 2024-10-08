e9ffa537ade28480d6d9afb7dafe9579891d23ec
Ishan Veer <ishanveer@Ishans-MacBook-Air.local>
2024-10-08 21:30:38 +0530

    fetching mock users and handling fetch more users

--- users/discord/components/UsersSection.js
+++ users/discord/components/UsersSection.js
@@ -4,9 +4,21 @@ export const UsersSection = ({
   users,
   showUser,
   handleUserSelected,
-  handleScroll,
-  isLoading,
+  fetchMoreUsers,
 }) => {
+  const handleScroll = (e) => {
+    console.log('scroll triggered');
+
+    const container = e.target;
+    const { scrollTop, scrollHeight, clientHeight } = container;
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      isLoading = true;
+      console.log('user scrolling');
+      fetchMoreUsers();
+    }
+  };
+
+  // handle scroll is not working - add event listener on every scroll element
   return createElement(
     'aside',
     {
@@ -14,26 +26,26 @@ export const UsersSection = ({
       onclick: handleUserSelected,
       onscroll: handleScroll,
     },
-    [
-      ...users?.map((user) => {
-        return createElement(
-          'div',
-          {
-            class: `user_card ${
-              users[showUser].id === user.id ? 'active_tab' : ''
-            }`,
-            data_key: user.id,
-          },
-          [
-            createElement('img', {
-              src: user?.picture?.url ?? dummyPicture,
-              class: 'user_image',
-            }),
-            createElement('span', {}, [user.first_name + ' ' + user.last_name]),
-          ],
-        );
-      }),
-      isLoading && createElement('div', 'Load more users...'),
-    ],
+    users?.map((user) => {
+      return createElement(
+        'div',
+        {
+          class: `user_card ${
+            users[showUser].id === user.id ? 'active_tab' : ''
+          }`,
+          data_key: user.id,
+        },
+        [
+          createElement('img', {
+            src: user?.picture?.url ?? dummyPicture,
+            class: 'user_image',
+          }),
+          // createElement('span', {}, [
+          //   user.first_name + ' ' + user.last_name + user.username,
+          // ]),
+          createElement('span', {}, [user.username]),
+        ],
+      );
+    }),
   );
 };
