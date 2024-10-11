6c1357445295c519372be746c46c9420b63cf063
shantanu-02 <shantanupawar101@gmail.com>
2024-10-11 20:20:37 +0530

    Fix: Minor fixes

--- footer/footerComponent.js
+++ footer/footerComponent.js
@@ -1,13 +1,10 @@
-// footerComponent.js
-
-// Define the loadFooter function that takes an href argument
-function loadFooter(href) {
+function loadFooter() {
   const footerHTML = `
       <footer class="footer" data-test-id="footer">
         <p class="info-repo" data-test-id="info-repo">
           The contents of this website are deployed from this
           <a
-            href="${href}"
+            href="https://github.com/Real-Dev-Squad/website-dashboard"
             target="_blank"
             rel="noopener noreferrer"
             data-test-id="repo-link"
@@ -18,6 +15,5 @@ function loadFooter(href) {
       </footer>
     `;
 
-  // Insert the footer into the DOM
   document.body.insertAdjacentHTML('beforeend', footerHTML);
 }
