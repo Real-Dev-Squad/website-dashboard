d5668bfd2b1907a6af656c2a905fa98b8a6136ef
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2024-10-25 23:02:55 +0530

    feat: add check for isSuperUser

--- discord-invite/script.js
+++ discord-invite/script.js
@@ -1,8 +1,40 @@
 // Script file for discord-invite feature.
+import { createElement, getIsSuperUser, showToast } from './utils.js';
+
 const discordInviteDescription = document.querySelector(
   '#discord-invite-link-description',
 );
 const createInviteButton = document.querySelector('#create-discord-invite');
+const loader = document.querySelector('.loader');
+const mainContainer = document.querySelector('.container');
+const subContainer = document.querySelector('.wrapper');
+
+function changeLoaderVisibility({ hide }) {
+  if (hide) loader.classList.add('hidden');
+  else loader.classList.remove('hidden');
+}
+
+(async function renderCardsInitial() {
+  changeLoaderVisibility({ hide: false });
+
+  const isSuperUser = await getIsSuperUser();
+
+  if (!isSuperUser) {
+    const unAuthorizedText = createElement({
+      type: 'h1',
+      attributes: { class: 'unauthorized-text' },
+      innerText: 'You are not authorized to view this page.',
+    });
+    mainContainer.append(unAuthorizedText);
+    subContainer.classList.add('hidden');
+    changeLoaderVisibility({ hide: true });
+    return;
+  }
+
+  subContainer.classList.remove('hidden');
+
+  changeLoaderVisibility({ hide: true });
+})();
 
 createInviteButton.addEventListener('click', () => {
   console.log('Invite button clicked', discordInviteDescription.value);
