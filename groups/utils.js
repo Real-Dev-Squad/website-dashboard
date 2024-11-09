de2e4aa0f1db87afc9762c512f09aee02a76a9aa
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-11-09 19:32:38 +0530

    feat: UI for deleting discord groups by super users (#909)
    * UI changes for delete group feature under feature flag
    
    * chore: Test cases
    
    * fix: issue is no group found

--- groups/utils.js
+++ groups/utils.js
@@ -1,5 +1,4 @@
 const BASE_URL = window.API_BASE_URL; // REPLACE WITH YOUR LOCALHOST URL FOR TESTING LOCAL BACKEND
-
 async function getMembers() {
   try {
     const res = await fetch(`${BASE_URL}/users/`, {
@@ -117,6 +116,33 @@ async function removeRoleFromMember(roleId, discordId) {
   }
 }
 
+async function deleteDiscordGroupRole(groupId, roleId) {
+  try {
+    const res = await fetch(
+      `${BASE_URL}/discord-actions/groups/${groupId}?dev=true`,
+      {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ roleid: roleId }),
+      },
+    );
+
+    if (!res.ok) {
+      const errorResponse = await res.json();
+      throw new Error(
+        `Failed to delete group role: ${JSON.stringify(errorResponse.error)}`,
+      );
+    }
+
+    return await res.json();
+  } catch (err) {
+    throw err;
+  }
+}
+
 function removeGroupKeywordFromDiscordRoleName(groupName) {
   if (/^group.*/.test(groupName)) {
     const splitNames = groupName.split('-');
@@ -164,6 +190,7 @@ export {
   createDiscordGroupRole,
   addGroupRoleToMember,
   removeRoleFromMember,
+  deleteDiscordGroupRole,
   removeGroupKeywordFromDiscordRoleName,
   getDiscordGroupIdsFromSearch,
   getParamValueFromURL,
