ffbedbe1d365d7ea43c0d58103bff399cf493a7a
Sunil Kumar <sunilkumar24.dev@gmail.com>
2024-11-16 16:52:57 +0500

    featureUrl removed

--- __tests__/tasks/task-dependency.test.js
+++ __tests__/tasks/task-dependency.test.js
@@ -22,25 +22,10 @@ describe('Input box', () => {
       await page.setRequestInterception(true);
       page.on('request', (interceptedRequest) => {
         const url = interceptedRequest.url();
-<<<<<<< HEAD
-<<<<<<< HEAD
-
         const mockResponses = {
           [`${STAGING_API_URL}/levels`]: levels,
           [`${STAGING_API_URL}/users`]: users,
           [`${STAGING_API_URL}/tags`]: tags,
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> aa0f2dc (refactored for readability)
-        const mockResponses = {
-          [`${API_BASE_URL}/levels`]: levels,
-          [`${API_BASE_URL}/users`]: users,
-          [`${API_BASE_URL}/tags`]: tags,
->>>>>>> 93a5221 (Added data-testid for testing,also added new tests)
-=======
->>>>>>> 14374bd (API_BASE_URL changed to stating for testing)
         };
 
         if (mockResponses[url]) {
@@ -181,14 +166,7 @@ describe('Input box', () => {
       );
       expect(display).toBe('none');
     });
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 93a5221 (Added data-testid for testing,also added new tests)
-=======
-
->>>>>>> aa0f2dc (refactored for readability)
     it('should display the dependsOn field in dev mode', async () => {
       const dependsOnField = await page.$('[data-testid="dependsOn"]');
       expect(dependsOnField).toBeTruthy();
