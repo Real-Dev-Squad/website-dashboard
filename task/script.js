2150687ef0684ae18eae1bd9de657dd2cdd074cc
Sunil Kumar <sunilkumar24.dev@gmail.com>
2024-11-17 13:19:31 +0500

    status=available typo fixed

--- task/script.js
+++ task/script.js
@@ -24,7 +24,7 @@ const taskLevelDiv = document
 if (isDev && taskLevelDiv) {
   taskLevelDiv.style.display = 'none';
 }
-console.log('hehehe');
+
 const category = document.getElementById('category');
 
 category.addEventListener('change', async () => {
@@ -161,16 +161,11 @@ taskForm.onsubmit = async (e) => {
   const {
     title,
     purpose,
-    featureUrl,
-    type,
     links,
     endsOn,
     status,
-    category,
-    level,
     dependsOn,
     assignee,
-    participants,
     priority,
     percentCompleted,
     completionAwardDinero,
@@ -179,6 +174,10 @@ taskForm.onsubmit = async (e) => {
     lossRateNeelam,
     isNoteworthy,
   } = getObjectOfFormData(taskForm);
+  if (!isDev) {
+    const { featureUrl, type, participants, category, level } =
+      getObjectOfFormData(taskForm);
+  }
 
   if (status === 'ASSIGNED' && !assignee.trim()) {
     alert('Assignee can not be empty');
@@ -217,7 +216,7 @@ taskForm.onsubmit = async (e) => {
     delete dataToBeSent.endsOn;
   }
 
-  if (status === StatusType.AVAILABLE) {
+  if (status === 'AVIALABLE') {
     delete dataToBeSent.endsOn;
   }
   if (isDev) {
@@ -265,7 +264,6 @@ taskForm.onsubmit = async (e) => {
   }
 
   try {
-    console.log(dataToBeSent);
     const response = await fetch(`${API_BASE_URL}/tasks?userStatusFlag=true`, {
       method: 'POST',
       credentials: 'include',
