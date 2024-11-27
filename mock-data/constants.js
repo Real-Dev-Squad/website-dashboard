e1f5451a8fbfbac3e99f2ee7da097f695d13eacb
AnujChhikara <anujchhikara777@gmail.com>
2024-11-27 11:42:45 +0530

    fix: properly declare constants

--- mock-data/constants.js
+++ mock-data/constants.js
@@ -1,6 +1,5 @@
-// constants.js
-STAGING_API_URL = 'https://staging-api.realdevsquad.com';
-LOCAL_TEST_PAGE_URL = 'http://localhost:8000';
+const STAGING_API_URL = 'https://staging-api.realdevsquad.com';
+const LOCAL_TEST_PAGE_URL = 'http://localhost:8000';
 
 module.exports = {
   STAGING_API_URL,
