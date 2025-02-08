75932829b60ed26a585bddf3ccfeb512f673f211
AnujChhikara <anujchhikara777@gmail.com>
2025-02-08 15:47:31 +0530

    feature/created onboarding tab

--- __tests__/requests/requests.test.js
+++ __tests__/requests/requests.test.js
@@ -4,6 +4,7 @@ const {
   requestActionResponse,
   approvedRequest,
   extensionRequest,
+  onboardingExtensionRequest,
 } = require('../../mock-data/requests');
 const { allUsersData } = require('../../mock-data/users');
 const {
@@ -67,6 +68,19 @@ describe('Tests the request cards', () => {
           body: JSON.stringify(extensionRequest),
         });
       } else if (
+        url === `${STAGING_API_URL}/requests?dev=true&type=ONBOARDING&size=12`
+      ) {
+        interceptedRequest.respond({
+          status: 200,
+          contentType: 'application/json',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+          body: JSON.stringify(onboardingExtensionRequest),
+        });
+      } else if (
         url === `${STAGING_API_URL}/requests/Wl4TTbpSrQDIjs6KLJwD?dev=true`
       ) {
         interceptedRequest.respond({
@@ -114,6 +128,11 @@ describe('Tests the request cards', () => {
 
     await page.click('.request__action__btn.accept__btn');
 
+    await page.waitForFunction(
+      () =>
+        document.querySelector('.request__status')?.textContent === 'Approved',
+    );
+
     await page.waitForSelector('.request__status');
     const updatedStatusButtonText = await page.$eval(
       '.request__status',
@@ -124,7 +143,7 @@ describe('Tests the request cards', () => {
 
   it('should load the extension request when the extension tab is clicked', async () => {
     await page.click('#extension_tab_link');
-    await page.waitForSelector('.ooo_request__card');
+    await page.waitForSelector('.request__card');
 
     const cardTitle = await page.$eval(
       '.request__content p',
@@ -137,5 +156,80 @@ describe('Tests the request cards', () => {
       (el) => el.textContent,
     );
     expect(statusButtonText).toBe('Approved');
+  });
+
+  it('should not render the onboarding extension tab when dev is not true', async () => {
+    const onboardingTabLink = await page.$('[data-testid="Onboarding-tab"]');
+    const hasHiddenClass = await onboardingTabLink.evaluate((el) =>
+      el.classList.contains('hidden'),
+    );
+    expect(hasHiddenClass).toBe(true);
+  });
+
+  it('should render the onboarding extensoin tab when dev is true', async () => {
+    await page.goto(`${LOCAL_TEST_PAGE_URL}/requests?dev=true`);
+    await page.waitForNetworkIdle();
+    const onboardingTabLink = await page.$('[data-testid="Onboarding-tab"]');
+    const hasHiddenClass = await onboardingTabLink.evaluate((el) =>
+      el.classList.contains('hidden'),
+    );
+    expect(hasHiddenClass).toBe(false);
+  });
+
+  it('should render the onboarding requests when onboarding tab is clicked', async () => {
+    await page.click('#onboarding_extension_tab_link');
+    await page.waitForSelector('[data-testid="request-card"]');
+
+    const statusButtonText = await page.$eval(
+      '[data-testid="request-status"]',
+      (el) => el.textContent,
+    );
+    expect(statusButtonText.toLowerCase()).toBe(
+      onboardingExtensionRequest.data[0].state.toLowerCase(),
+    );
+  });
+
+  it('should render approve/reject buttons and superuser details correctly based on status', async () => {
+    await page.goto(`${LOCAL_TEST_PAGE_URL}/requests?dev=true`);
+    const onboardingTabLink = await page.$('[data-testid="Onboarding-tab"]');
+    await onboardingTabLink.click();
+    await page.waitForSelector('[data-testid="request-card"]');
+
+    const requestCards = await page.$$('[data-testid="request-card"]');
+    expect(requestCards.length).toBeGreaterThan(0);
+
+    for (const card of requestCards) {
+      const statusText = await card.$eval(
+        '[data-testid="request-status"]',
+        (el) => el.textContent.trim(),
+      );
+
+      const actionContainer = await card.$('[data-testid="action-container"]');
+      const superuserSection = await card.$(
+        '[data-testid="admin-info-and-status"]',
+      );
+
+      const isActionContainerHidden = await actionContainer.evaluate((el) =>
+        el.classList.contains('hidden'),
+      );
+      const isSuperuserSectionHidden = await superuserSection.evaluate((el) =>
+        el.classList.contains('hidden'),
+      );
+
+      expect(isActionContainerHidden).toBe(statusText !== 'Pending');
+      expect(isSuperuserSectionHidden).toBe(statusText === 'Pending');
+
+      if (statusText === 'Pending') {
+        const approveButton = await card.$('[data-testid="approve-button"]');
+        const rejectButton = await card.$('[data-testid="reject-button"]');
+        const remarkInput = await card.$(
+          '[data-testid="request-remark-input"]',
+        );
+
+        expect(approveButton).toBeTruthy();
+        expect(rejectButton).toBeTruthy();
+        expect(remarkInput).toBeTruthy();
+      }
+    }
   });
 });
--- requests/constants.js
+++ requests/constants.js
@@ -7,9 +7,11 @@ const Status = {
 
 const OOO_REQUEST_TYPE = 'OOO';
 const EXTENSION_REQUEST_TYPE = 'EXTENSION';
+const ONBOARDING_EXTENSION_REQUEST_TYPE = 'ONBOARDING';
 const REQUEST_CONTAINER_ID = 'request_container';
 const OOO_TAB_ID = 'ooo_tab_link';
 const EXTENSION_TAB_ID = 'extension_tab_link';
+const ONBOARDING_EXTENSION_TAB_ID = 'onboarding_extension_tab_link';
 
 const DEFAULT_DATE_FORMAT = 'DD MMM YYYY';
 
@@ -24,6 +26,7 @@ const ErrorMessages = {
   UNAUTHORIZED: 'You are unauthorized to view this section',
   OOO_NOT_FOUND: 'OOO Requests not found',
   EXTENSION_NOT_FOUND: 'Extension Requests not found',
+  ONBOARDING_EXTENSION_NOT_FOUND: 'Onboarding extension Requests not found',
   SERVER_ERROR: 'Unexpected error occurred',
 };
 
